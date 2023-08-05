72ca1b43b768c7f407176a45fb77fa179d7a18fe
ZaaXxyZzModDev <130498570+ZaaXxyZzModDev@users.noreply.github.com>
2023-08-05 15:18:14 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -3,10 +3,10 @@ __path = process.cwd()
 var express = require('express');
 var router = express.Router();
 
-router.get('/docs', (req, res) => {
+router.get('/', (req, res) => {
     res.sendFile(__path + '/views/docs-1.html')
 })
-router.get('/', (req, res) => {
+router.get('/docs', (req, res) => {
     res.sendFile(__path + '/views/docs.html')
 })
 router.get('/api/game', (req, res) => {
